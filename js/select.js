6ce1b135887fc3ccd0f94082bbcad9256515baac
Ella0707 <pakelvi77@gmail.com>
2022-02-22 22:50:01 +0300

    first

---
+++ js/select.js
@@ -0,0 +1,30 @@
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select-header');
+    let selectItem = document.querySelectorAll('.select-body');
+
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('active');
+    }
+
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.select__current');
+        currentText.innerText = text;
+        select.classList.remove('active');
+
+    }
+
+};
+
+
+select();
+
