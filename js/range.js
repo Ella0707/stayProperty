4072ef30137eda857eb651d59c0fee9c7e2550d7
Ella0707 <pakelvi77@gmail.com>
2022-02-28 22:28:33 +0300

    mortgage

--- js/range.js
+++ js/range.js
@@ -1,10 +1,40 @@
-$(".js-range-slider").ionRangeSlider({
+$(".room-cost").ionRangeSlider({
     // type: "double",
-    // min: 1000,
-    // max: 1000000,
+    min: 1000,
+    max: 50000000,
     skin: "round",
-    from: 1000,
-    to: 1000000,
+    // from: 1000,
+    // to: 1000000,
     grid: false,
     postfix: " €",
+    hide_min_max: true,
+    force_edges: true,
+});
+
+
+$(".payment").ionRangeSlider({
+    // type: "double",
+    min: 1000,
+    max: 40000,
+    skin: "round",
+    // from: 1000,
+    // to: 1000000,
+    grid: false,
+    postfix: " €",
+    hide_min_max: true,
+    force_edges: true,
+});
+
+
+$(".term").ionRangeSlider({
+    // type: "double",
+    min: 1,
+    max: 30,
+    skin: "round",
+    // from: 1000,
+    // to: 1000000,
+    grid: false,
+    postfix: " лет",
+    hide_min_max: true,
+    force_edges: true,
 });
