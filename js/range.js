9de85d729dd75bf4a64ac3e9cc7cbce2a1bfff12
unknown <pakelvi0707@gmail.com>
2022-03-01 16:50:22 +0300

    real-estate

--- js/range.js
+++ js/range.js
@@ -38,3 +38,82 @@ $(".term").ionRangeSlider({
     hide_min_max: true,
     force_edges: true,
 });
+
+
+// $(".price").ionRangeSlider({
+//     type: "double",
+//     min: 10000,
+//     max: 300000000,
+//     skin: "round",
+//     // from: 1000,
+//     // to: 1000000,
+//     grid: false,
+//     postfix: " €",
+//     hide_min_max: true,
+//     force_edges: true,
+//     values_separator: "  ",
+// });
+
+var $range = $(".price"),
+    $inputFrom = $(".price-from"),
+    $inputTo = $(".price-to"),
+    instance,
+    min = 0,
+    max = 10000000,
+    from = 0,
+    to = 0;
+   
+
+$range.ionRangeSlider({
+	skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    // from: 200,
+    // to: 800,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,
+    postfix: " €",
+    hide_min_max: true,
+    force_edges: true,
+});
+instance = $range.data(".price");
+
+function updateInputs (data) {
+	from = data.from;
+    to = data.to;
+    
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < min) {
+        val = min;
+    } else if (val > to) {
+        val = to;
+    }
+    
+    instance.update({
+        from: val
+    });
+});
+
+$inputTo.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < from) {
+        val = from;
+    } else if (val > max) {
+        val = max;
+    }
+    
+    instance.update({
+        to: val
+    });
+});
